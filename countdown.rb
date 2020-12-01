90965fb4cf1cadc9c11d519b974a82584edd0388
Keith Filkins <kafilkins@me.com>
2020-12-01 21:06:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,3 +6,11 @@ def countdown(integer)
     integer == 0
     return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(integer)
+  while integer > 0
+    puts "#{integer} SECOND(S)!"
+    integer -= 1
+    sleep(5)
+    end
+end
